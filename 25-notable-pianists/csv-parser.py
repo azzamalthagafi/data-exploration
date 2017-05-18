a249e21e4915f99269089cfe183ea1baa39cea11
azzamalthagafi <azzam.thagafi@gmail.com>
2017-05-18 14:41:32 -0400

    Implemented and tested csv-parser.py successfully

---
+++ 25-notable-pianists/csv-parser.py
@@ -0,0 +1,48 @@
+"""
+
+Parses a csv file and performs frequency analysis on the notes
+played then writes the result to a .txt file.
+
+
+Example:
+
+	$ python csv-parser.py <file.csv>
+
+	running the previous line will output to a nfa_<file>.csv file
+	matching each note with the number of times it has been played in the
+	.mid file
+
+Dependencies: 
+	pandas
+
+"""
+
+import sys, os, pandas as pd
+
+__author__ = "Azzam Althagafi"
+__copyright__ = "Copyright 2017, Azzam Althagafi"
+__license__ = "MIT"
+__email__ = "aazzam@seas.upenn.edu"
+
+# commandline arguments
+filename = sys.argv[1]
+
+# When importing csv, we name columns to avoid tokenizing data error
+# This occurs because Time_signature events have 7 columns, while notes have
+# 6 columns.
+my_cols = [1, 2, "event", 4, "note", 6, 7]
+df = pd.read_csv(filename, names=my_cols)
+
+# According to midicsv, a note is either "Note_on_c" or "Note_off_c"
+# notes contains an array of all the notes played in chronological order.
+note_events = df[(df.event == " Note_on_c") | (df.event == " Note_off_c")]
+notes = note_events.note
+
+note_frequencies = notes.value_counts()
+
+# file writing
+note_frequencies.to_csv("nfa_" + os.path.splitext(filename)[0] + ".csv")
+
+
+
+
